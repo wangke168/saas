4afc7dde379f097e95d260889118edaef3c0b185
王科 <wangke@wangkedeMac-mini.local>
2026-01-08 20:06:17 +0800

    美团测试

--- app/Http/Controllers/Webhooks/MeituanController.php
+++ app/Http/Controllers/Webhooks/MeituanController.php
@@ -424,7 +424,7 @@ class MeituanController extends Controller
                 ]);
                 // 根据文档，订单创建V2失败响应格式：code、describe、partnerId（无body字段）
                 // 接口是全局加密的，所以响应需要加密
-                return $this->errorResponse(410, '未开始售卖该日期或场次，此产品的购买规则为XXX', $partnerId, true);
+                return $this->errorResponse(410, '每笔订单需要X个游玩人信息', $partnerId, true);
             }
             // ============================================
             // 测试代码结束
